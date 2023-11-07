30407c92fa7e560eb51d784dac92a68302eee364
AlenaPanPanda <guiyguker@gmail.com>
2023-11-07 11:30:48 +0100

    check first number: toast, error message swap button and final number

--- app/src/main/java/com/example/numberbaseconverter/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseconverter/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.numberbaseconverter
 
 import android.R
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.numberbaseconverter.databinding.ActivityMainBinding
 import java.math.BigDecimal
 import java.math.BigInteger
@@ -34,7 +36,7 @@ class SymbolsOfNumber() {
         return listSymbols
     }
 
-    fun getListNumbersInARow(): List<Char> {
+    private fun getListNumbersInARow(): List<Char> {
         val listNumbers = mutableListOf<Char>()
 
         for (c in '0'..'9') listNumbers.add(c)
@@ -65,6 +67,11 @@ class MainActivity : AppCompatActivity() {
 
         val listOfBases = Bases().createListOfBases()
 
+        binding.startNumberEditText.setOnClickListener {
+            binding.finalNumber.text = ""
+            binding.startNumberTextField.error = null
+        }
+
         ArrayAdapter(
             this,
             R.layout.simple_spinner_item,
@@ -86,23 +93,13 @@ class MainActivity : AppCompatActivity() {
             binding.targetBaseSpinner.adapter = adapter
         }
 
-        binding.startNumber.setOnClickListener {
-            binding.finalNumber.text = ""
-        }
-
-        // final number disappear when start number is edited
-
-        // add check first number: correct symbols, number of dots, correspond to base
-
-        // change button and targetNumber in xml
-
         // create pretty spinners
 
         // change app icon
 
         // change app theme
 
-        // add chose to upper or lower letters
+        // add chose to upper or .
 
         // add share button
 
@@ -116,155 +113,167 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener {
             val startBase = binding.startBaseSpinner.selectedItem.toString().toBigInteger()
             val targetBase = binding.targetBaseSpinner.selectedItem.toString().toBigInteger()
-            val startNumber = binding.startNumber.text.toString()
+            val startNumber = binding.startNumberEditText.text.toString()
 
-            binding.finalNumber.text = doConversion(startBase, targetBase, startNumber)
-        }
-    }
-}
-
-
-fun doConversion(startBase: BigInteger, targetBase: BigInteger, startNumber: String): String {
-    val firstPartOfNumber = getFirstPartOfNumber(startNumber)
-    val decimalResultFirstPart = commonConversionToDecimal(firstPartOfNumber.uppercase(), startBase)
-    val targetResultFirstPart = commonConversionFromDecimal(decimalResultFirstPart, targetBase)
-    var targetNumber = targetResultFirstPart
-
-    if (targetResultFirstPart.isEmpty()) {
-        targetNumber += "0"
-    }
-
-    if (isFractionalNumber(startNumber)) {
-        val decimalReminderResult =
-            commonConversionReminderToDecimal(startNumber, startBase.toBigDecimal())
-        val targetReminder =
-            commonConversionReminderFromDecimal(decimalReminderResult, targetBase.toBigDecimal())
-        targetNumber += ".$targetReminder"
-    }
-
-    return targetNumber.lowercase()
-}
-
-fun getFirstPartOfNumber(number: String): String = number.split(".").first()
-
-fun getReminderPartOfNumber(number: String): String {
-    return number.split('.').last()
-}
-
-fun isFractionalNumber(number: String): Boolean {
-    return number.contains('.')
-}
-
-fun commonConversionToDecimal(sourceNumber: String, sourceBase: BigInteger): BigInteger {
-    var dec = BigInteger("0")
-    for (index in sourceNumber.indices) {
-        val symbol = sourceNumber[index].uppercase()
-        val n = AllNumbers.indexOf(symbol)
-        val pow = sourceNumber.length - index - 1
-        val c = sourceBase.pow(pow)
-        dec += (n.toBigInteger() * c)
-    }
-
-    return dec
-}
-
-fun commonConversionFromDecimal(number: BigInteger, base: BigInteger): String {
-    val zero = BigInteger("0")
-    var dec = number
-    var targetNumber = ""
-    while (dec != zero) {
-        val rem = dec % base
-        val symbol = AllNumbers[rem.toInt()]
-        targetNumber += symbol
-        dec /= base
-    }
-
-    return targetNumber.reversed()
-}
-
-fun commonConversionReminderToDecimal(number: String, sourceBase: BigDecimal): BigDecimal {
-    var dec = BigDecimal.ZERO
-    val one = BigDecimal(1.0)
-    val sourceReminderPart = getReminderPartOfNumber(number)
-    for (index in sourceReminderPart.indices) {
-        val symbol = sourceReminderPart[index].uppercase()
-        val n = AllNumbers.indexOf(symbol)
-        val pow = index + 1
-        val basePow = sourceBase.pow(pow)
-        val c = one.divide(basePow, 10, RoundingMode.HALF_UP)
-        dec += (n.toBigDecimal() * c)
-    }
-
-    return dec.divide(BigDecimal.ONE, 5, RoundingMode.HALF_UP)
-}
-
-fun commonConversionReminderFromDecimal(number: BigDecimal, targetBase: BigDecimal): String {
-    val intNumber = number.toInt()
-    var reminderDec = number - intNumber.toBigDecimal()
-    var reminderTarget = "0."
-
-    repeat(5) {
-        val n = reminderDec * targetBase
-        val firstPart = getFirstPartOfNumber(n.toString())
-        val symbolFirstPart = AllNumbers[firstPart.toInt()]
-        reminderTarget += symbolFirstPart
-        reminderDec = n - firstPart.toBigDecimal()
-    }
-
-    return getReminderPartOfNumber(reminderTarget)
-}
-
-fun checkStartNumber(number: String, base: Int, listOfBases: List<BigInteger>): Boolean {
-
-
-    if (!isAllSymbolsAreCorrect(number)) {
-        //show toast
-        //show validation
-        //return false
-    }
-
-    if (!isNullOrOneDots(number)) {
-        //show toast
-        //show validation
-        //return false
-    }
-
-    if (!isNumberCorrespondsBase(number, base, listOfBases)) {
-        //show toast
-        //show validation
-        //return false
-    }
-
-    return true
-}
-
-fun isAllSymbolsAreCorrect(number: String): Boolean {
-    val listOfCorrectSymbols = SymbolsOfNumber().getAllCorrectSymbols()
-
-    for (symbol in number) {
-        if (!listOfCorrectSymbols.contains(symbol)) return false
-    }
-
-    return true
-}
-
-fun isNullOrOneDots(number: String): Boolean {
-    val numberOfDots = number.count { it == '.' }
-    if (numberOfDots == 0 || numberOfDots == 1) return true
-
-    return false
-}
-
-fun isNumberCorrespondsBase(number: String, base: Int, listOfBases: List<BigInteger>): Boolean {
-    val listCorrectSymbolsThisBase = SymbolsOfNumber().getListOfSymbolsThisBase(base)
-
-    for (symbol in number) {
-        if (symbol != '.') {
-            if (!listCorrectSymbolsThisBase.contains(symbol.uppercaseChar())) return false
+            if (checkStartNumber(startNumber, startBase.toInt())) {
+                binding.finalNumber.text = doConversion(startBase, targetBase, startNumber)
+            }
         }
     }
 
-    return true
+
+    private fun doConversion(startBase: BigInteger, targetBase: BigInteger, startNumber: String): String {
+        val firstPartOfNumber = getFirstPartOfNumber(startNumber)
+        val decimalResultFirstPart =
+            commonConversionToDecimal(firstPartOfNumber.uppercase(), startBase)
+        val targetResultFirstPart = commonConversionFromDecimal(decimalResultFirstPart, targetBase)
+        var targetNumber = targetResultFirstPart
+
+        if (targetResultFirstPart.isEmpty()) {
+            targetNumber += "0"
+        }
+
+        if (isFractionalNumber(startNumber)) {
+            val decimalReminderResult =
+                commonConversionReminderToDecimal(startNumber, startBase.toBigDecimal())
+            val targetReminder =
+                commonConversionReminderFromDecimal(
+                    decimalReminderResult,
+                    targetBase.toBigDecimal()
+                )
+            targetNumber += ".$targetReminder"
+        }
+
+        return targetNumber.lowercase()
+    }
+
+    private fun getFirstPartOfNumber(number: String): String = number.split(".").first()
+
+    private fun getReminderPartOfNumber(number: String): String {
+        return number.split('.').last()
+    }
+
+    private fun isFractionalNumber(number: String): Boolean {
+        return number.contains('.')
+    }
+
+    private fun commonConversionToDecimal(sourceNumber: String, sourceBase: BigInteger): BigInteger {
+        var dec = BigInteger("0")
+        for (index in sourceNumber.indices) {
+            val symbol = sourceNumber[index].uppercase()
+            val n = AllNumbers.indexOf(symbol)
+            val pow = sourceNumber.length - index - 1
+            val c = sourceBase.pow(pow)
+            dec += (n.toBigInteger() * c)
+        }
+
+        return dec
+    }
+
+    private fun commonConversionFromDecimal(number: BigInteger, base: BigInteger): String {
+        val zero = BigInteger("0")
+        var dec = number
+        var targetNumber = ""
+        while (dec != zero) {
+            val rem = dec % base
+            val symbol = AllNumbers[rem.toInt()]
+            targetNumber += symbol
+            dec /= base
+        }
+
+        return targetNumber.reversed()
+    }
+
+    private fun commonConversionReminderToDecimal(number: String, sourceBase: BigDecimal): BigDecimal {
+        var dec = BigDecimal.ZERO
+        val one = BigDecimal(1.0)
+        val sourceReminderPart = getReminderPartOfNumber(number)
+        for (index in sourceReminderPart.indices) {
+            val symbol = sourceReminderPart[index].uppercase()
+            val n = AllNumbers.indexOf(symbol)
+            val pow = index + 1
+            val basePow = sourceBase.pow(pow)
+            val c = one.divide(basePow, 10, RoundingMode.HALF_UP)
+            dec += (n.toBigDecimal() * c)
+        }
+
+        return dec.divide(BigDecimal.ONE, 5, RoundingMode.HALF_UP)
+    }
+
+    private fun commonConversionReminderFromDecimal(number: BigDecimal, targetBase: BigDecimal): String {
+        val intNumber = number.toInt()
+        var reminderDec = number - intNumber.toBigDecimal()
+        var reminderTarget = "0."
+
+        repeat(5) {
+            val n = reminderDec * targetBase
+            val firstPart = getFirstPartOfNumber(n.toString())
+            val symbolFirstPart = AllNumbers[firstPart.toInt()]
+            reminderTarget += symbolFirstPart
+            reminderDec = n - firstPart.toBigDecimal()
+        }
+
+        return getReminderPartOfNumber(reminderTarget)
+    }
+
+    @SuppressLint("ShowToast")
+    fun checkStartNumber(number: String, base: Int): Boolean {
+        if (!isAllSymbolsAreCorrect(number)) {
+            val text = "Symbols are not correct"
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+            binding.startNumberTextField.error = text
+            return false
+        }
+
+        if (!isNullOrOneDots(number)) {
+            val text = "Do not use more than one dot"
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+            binding.startNumberTextField.error = text
+            return false
+        }
+
+        if (!isNumberCorrespondsBase(number, base)) {
+            val text = "Number does not correspond to start base"
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+            binding.startNumberTextField.error = text
+            return false
+        }
+
+        if (number == ""){
+            val text = "Number to convert is empty"
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+            binding.startNumberTextField.error = text
+            return false
+        }
+
+        return true
+    }
+
+    private fun isAllSymbolsAreCorrect(number: String): Boolean {
+        val listOfCorrectSymbols = SymbolsOfNumber().getAllCorrectSymbols()
+
+        for (symbol in number) {
+            if (!listOfCorrectSymbols.contains(symbol)) return false
+        }
+
+        return true
+    }
+
+    private fun isNullOrOneDots(number: String): Boolean {
+        val numberOfDots = number.count { it == '.' }
+        if (numberOfDots == 0 || numberOfDots == 1) return true
+
+        return false
+    }
+
+    private fun isNumberCorrespondsBase(number: String, base: Int): Boolean {
+        val listCorrectSymbolsThisBase = SymbolsOfNumber().getListOfSymbolsThisBase(base)
+
+        for (symbol in number) {
+            if (symbol != '.') {
+                if (!listCorrectSymbolsThisBase.contains(symbol.uppercaseChar())) return false
+            }
+        }
+        return true
+    }
 }
-
-
